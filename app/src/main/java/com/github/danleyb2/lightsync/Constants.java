1e2954624c951b552a1fb852f43f9bfb612ac7ac
Nyaundi Brian <ndieksman@gmail.com>
2016-09-14 10:27:04 +0300

    first commit

---
+++ app/src/main/java/com/github/danleyb2/lightsync/Constants.java
@@ -0,0 +1,6 @@
+package com.github.danleyb2.lightsync;
+
+public class Constants {
+    //public static final String CHAT_SERVER_URL = "http://chat.socket.io";
+    public static final String CHAT_SERVER_URL = "https://lightsync.herokuapp.com/";
+}
---
+++ app/src/main/java/com/github/danleyb2/lightsync/model/Model.java
@@ -0,0 +1,27 @@
+package com.github.danleyb2.lightsync.model;
+
+/**
+ * Created by ndiek on 9/9/2016.
+ */
+public class Model {
+
+    private String id;
+    private String content;
+
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
